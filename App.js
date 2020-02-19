e7a6693016509f34492682804e9b604c8391329d
Tim MWandha <Tim Mwandha>
2020-02-19 12:58:30 +0300

    added toolben

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View,Text } from 'react-native';
 
 import Login from './components/Login';
 //import SignUp from './components/SignUp';
-import ToolBinCard from './components/ToolBinCard';
+import ToolBinCard from './components/toolben';
 //import Personnel from './components/Personnel';
 
 export default function App() {
   return (
     <View style={{ flex: 1 }}>
       {/* <Login /> */}
+      {/* <Text>fghfhf</Text> */}
       {/* <SignUp /> */}
       <ToolBinCard /> 
       {/*<Personnel /> */}
