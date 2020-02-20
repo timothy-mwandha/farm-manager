517b49dc7a9f4037a0e68af6d84a8461d1dd1d76
Tim MWandha <Tim Mwandha>
2020-02-20 12:50:21 +0300

    recreate toolbincard component copied from toolbencard

--- App.js
+++ App.js
@@ -3,16 +3,16 @@ import { View,Text } from 'react-native';
 
 import Login from './components/Login';
 //import SignUp from './components/SignUp';
-import ToolBinCard from './components/toolben';
+//import ToolBinCard from './components/toolben';
 //import Personnel from './components/Personnel';
 
 export default function App() {
   return (
     <View style={{ flex: 1 }}>
-      {/* <Login /> */}
+      <Login />
       {/* <Text>fghfhf</Text> */}
       {/* <SignUp /> */}
-      <ToolBinCard /> 
+      {/*<ToolBinCard /> */}
       {/*<Personnel /> */}
     </View>
   )
