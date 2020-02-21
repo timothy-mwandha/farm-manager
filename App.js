21b8e956ebd008511b533bab9b97ab2fafe7e4dc
Tim MWandha <Tim Mwandha>
2020-02-21 11:33:14 +0300

    latest commit

--- App.js
+++ App.js
@@ -4,16 +4,16 @@ import { View,Text } from 'react-native';
 import Login from './components/Login';
 //import SignUp from './components/SignUp';
 //import ToolBinCard from './components/toolben';
-//import Personnel from './components/Personnel';
+import Personnel from './components/Personnel';
 
 export default function App() {
   return (
     <View style={{ flex: 1 }}>
-      <Login />
+      {/*<Login /> */}
       {/* <Text>fghfhf</Text> */}
       {/* <SignUp /> */}
       {/*<ToolBinCard /> */}
-      {/*<Personnel /> */}
+      <Personnel />
     </View>
   )
 }
