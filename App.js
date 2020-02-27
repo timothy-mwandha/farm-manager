c31eadac7f619e4c3df39059443e648978d9429b
Irenyak1 <inyakate@gmail.com>
2020-02-27 17:43:53 +0300

    Add after presentation

--- App.js
+++ App.js
@@ -28,49 +28,44 @@ import { createStackNavigator } from "@react-navigation/stack";
 import SignUp from "./components/SignUp";
 import Login from "./components/Login";
 import PayRoll from "./components/PayRoll";
-
 import PasswordRecoveryOne from "./components/PasswordRecoveryOne";
 import PasswordRecoveryTwo from "./components/PasswordRecoveryTwo";
-
 import Advance from "./components/Advance";
-
 
 const Stack = createStackNavigator();
 
 function App() {
   return (
-
-    <View style={{ flex: 1 }}>
-      {/* <Login /> */}
-      {/* <SignUp /> */}
-      {/* <PayRoll /> */}
-      {/* <PasswordRecoveryOne /> */}
-      <PasswordRecoveryTwo />
-    </View>
+    // <View style={{ flex: 1 }}>
+    //   {/* <Login /> */}
+    //   {/* <SignUp /> */}
+    //   {/* <PayRoll /> */}
+    //   {/* <PasswordRecoveryOne /> */}
+    //   <PasswordRecoveryTwo />
+    // </View>
 
     <NavigationContainer>
       <Stack.Navigator
         initialRouteName="Login"
         screenOptions={{ gestureEnabled: false }}
       >
-        <Stack.Screen
-          name="Advance"
-          component={Advance}
-          options={{ headerShown: true }}
-        />
-        <Stack.Screen
+        {/* <Stack.Screen
           name="login"
           component={Login}
           initialParams={{ user: "me" }}
-        />
+        /> */}
         <Stack.Screen
           name="signup"
           component={SignUp}
           options={{ headerShown: true }}
         />
+        <Stack.Screen
+          name="Advance"
+          component={Advance}
+          options={{ headerShown: true }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
-
   );
 }
 
