efd043475e8a62907520849aa0a1b67372c0a02f
steven <kawooyastevenug@gmail.com>
2020-02-28 18:47:04 +0300

    complete working bottom nav bar awaiting pages

--- App.js
+++ App.js
@@ -21,9 +21,9 @@
 
 import * as React from "react";
 import { Text, View, StyleSheet } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { createStackNavigator } from "@react-navigation/stack";
+// import { NavigationContainer } from "@react-navigation/native";
+// import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+// import { createStackNavigator } from "@react-navigation/stack";
 
 import SignUp from "./components/SignUp";
 import Login from "./components/Login";
@@ -48,30 +48,30 @@ function App() {
       <PasswordRecoveryTwo />
     </View>
 
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Login"
-        screenOptions={{ gestureEnabled: false }}
-      >
-        <Stack.Screen
-          name="Advance"
-          component={Advance}
-          options={{ headerShown: true }}
-        />
-        <Stack.Screen
-          name="login"
-          component={Login}
-          initialParams={{ user: "me" }}
-        />
-        <Stack.Screen
-          name="signup"
-          component={SignUp}
-          options={{ headerShown: true }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+//     <NavigationContainer>
+//       <Stack.Navigator
+//         initialRouteName="Login"
+//         screenOptions={{ gestureEnabled: false }}
+//       >
+//         <Stack.Screen
+//           name="Advance"
+//           component={Advance}
+//           options={{ headerShown: true }}
+//         />
+//         <Stack.Screen
+//           name="login"
+//           component={Login}
+//           initialParams={{ user: "me" }}
+//         />
+//         <Stack.Screen
+//           name="signup"
+//           component={SignUp}
+//           options={{ headerShown: true }}
+//         />
+//       </Stack.Navigator>
+//     </NavigationContainer>
 
-  );
-}
+//   );
+// }
 
 export default App;
