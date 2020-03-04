3bbc003f34fccac028c6ae67343b22adcf14ca5f
steven <kawooyastevenug@gmail.com>
2020-03-04 16:49:40 +0300

    Landing pages for the finance and inventory modules

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,5 +1,5 @@
-import React, { Component }from "react";
-import { View, Text } from "react-native";
+import React, { Component } from "react";
+import { Text, View, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
@@ -20,27 +20,37 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 // import RequisitionForm from "./components/RequisitionForm";
 import LandReportInventory from './src/components/LandReportInventory'
 
+import SignUp from "./components/SignUp";
+import Login from "./components/Login";
+import PayRoll from "./components/PayRoll";
+import PasswordRecoveryOne from "./components/PasswordRecoveryOne";
+import PasswordRecoveryTwo from "./components/PasswordRecoveryTwo";
+import Advance from "./components/Advance";
+import ToolBinCard from './components/ToolBinCard';
+import Personnel from './components/Personnel';
+import ExpenditureForm from './components/ExpenditureForm';
+import Harvest from './components/Harvest';
+import HomePage from './src/components/HomePage'
+
+
+// export default function App() {
 export default class App extends Component {
   render() {
     return (
-      <View style={{ flex: 1 }}>
-        {/* <HomePage /> */}
-        {/* <Login /> */}
-        {/* <SignUp /> */}
-        {/* <PayRoll /> */}
-        {/* <PasswordRecoveryOne /> */}
-        {/* <PasswordRecoveryTwo /> */}
-        {/* <Advance/> */}
-        {/* <Personnel /> */}
-        {/* <ToolBinCard /> */}
-        {/* <ExpenditureForm /> */}
-        {/* <IncomeForm/> */}
-        {/* <BottomNav /> */}
-        {/* <Harvest /> */}
-        {/* <Consumable/> */}
-        {/* <RequisitionForm/> */}
-        <LandReportInventory />
-      </View>
+      // <View style={{ flex: 1 }}>
+      //   <Harvest/>
+      // <Personnel />
+      // <ToolBinCard />
+      // {/* <Login /> */}
+      // {/* <SignUp /> */}
+      // {/* <PayRoll /> */}
+      // {/* <PasswordRecoveryOne /> */}
+      // {/* <PasswordRecoveryTwo /> */}
+      // <ExpenditureForm />
+      // {/* <IncomeForm/> */}
+      // {/* <BottomNav /> */}
+
+      // </View>
     );
   }
 }
